bae84942e22461ba607737872dcf315517dc8fc5
tom <1501832474@qq.com>
2018-10-16 07:58:16 +0800

    Customize theme.

--- app/ftreact/config-overrides.js
+++ app/ftreact/config-overrides.js
@@ -2,13 +2,16 @@ const { injectBabelPlugin } = require('react-app-rewired')
 const rewireLess = require('react-app-rewire-less')
 
 module.exports = function override(config, env) {
-  console.log('load overides...')
+  console.log('Load customized theme overides...')
   config = injectBabelPlugin(
     ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], // change importing css to less
     config,
   )
   config = rewireLess.withLoaderOptions({
-    modifyVars: { '@primary-color': '#1DA57A' },
+    modifyVars: {
+      '@primary-color': '#1DA57A',
+      '@input-bg': 'transparent',
+    },
     javascriptEnabled: true,
   })(config, env)
   return config
